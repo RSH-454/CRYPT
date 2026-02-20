97f535aff8e7c91fa5790076440f5dee6f52037c
RSH-454 <rayansalhulaibah@gmail.com>
2026-02-20 04:53:41 +0300

    Fixes & Restructuring
    Fixed the ViewSaved() function, added comments to functions, and adjusted code structure.

--- Password Saver.py
+++ Password Saver.py
@@ -1,31 +1,34 @@
-print('[PASSWWORD SAVER]')
-input('Press enter to continue...\n')
-Option = input('Choose option: \n 1. Save new password \n 2. View saved passwords \n 3. Exit\nOption: ') 
-
 FileName = 'SavedData.txt'
+#This function allows the user to save new data.
 def SaveNew():
     print('Enter name of the account: ')
     Account = input()
     print('Enter password: ')
     Password = input()
     with open(FileName, 'a') as f:
-        f.write("Account: " + Account + " " + "Password: " + Password + "\n")  
+        f.write("Account: " + Account + " " + "Password: " + Password + "\n")
 
+#This function allows the user to view previously saved data. 
 def ViewSaved():
     print('Enter the name of the account you want to view: ')
     Account = input()
     with open(FileName, 'r') as f:
         content = f.read()
-        if Account in content:
-            print(content)
+        for line in content.splitlines():  
+         if Account in line:
+            print(line)    
+            break
         else:
-            print("Account not found.")
+          print("Account not found.")
 
-if Option == '1':
-    SaveNew()
+print('[PASSWORD SAVER]')
+input('Press enter to continue...\n')
+while True:
+    Option = input('Choose option: \n 1. Save new password \n 2. View saved passwords \n 3. Exit\nOption: ')
 
-elif Option == '2':
-    ViewSaved()
-
-elif Option == '3':
-    exit()
+    if Option == '1':
+        SaveNew()
+    elif Option == '2':
+        ViewSaved()
+    elif Option == '3':
+        break
